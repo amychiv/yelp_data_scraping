237914a22262f3dc085b9a1cb037965580d5e70c
cmoy11 <97237787+cmoy11@users.noreply.github.com>
2022-06-03 12:39:58 -0400

    Add files via upload

--- yelp_reviews/yelp_review_scraper.py
+++ yelp_reviews/yelp_review_scraper.py
@@ -41,9 +41,9 @@ def hyphenate(stripped_restaurants):
 def get_reviews(hyphen_restaurants, city):
     non_valid = []
     reviews = {}
-    for restaurant in hyphen_restaurants[-11:]:
+    for restaurant in hyphen_restaurants[-12:-10]:
         print(restaurant)
-        # create the restaurant's url using the hyphenated names
+        # create the restaurant's url using the hyphenated names and provided city
         url = f"https://www.yelp.com/biz/{restaurant}-{city}"
         soup = BeautifulSoup(requests.get(url).text, 'html.parser')
 
@@ -56,19 +56,18 @@ def get_reviews(hyphen_restaurants, city):
         first_ten_stars = soup.find_all('div', class_="i-stars__09f24__M1AR7")
         num_reviews_extract = soup.find_all('span', class_='css-1fdy0l5')
 
-        # skips restaurant if not in city
+        # skips restaurant if not in city/url does not exist
         if first_ten == []:
             print(f"{restaurant} is not in {city}")
             non_valid.append(restaurant)
             continue 
 
-        # get the number of reviews using regex
+        # get the number of reviews using regex expression
         for review in num_reviews_extract:
             reg_input = re.findall(regex, review.text)
             if reg_input != []:
                 num_reviews = int(reg_input[0])
                 review_information['number of reviews'] = num_reviews
-
         print(f"{restaurant} has {num_reviews} reviews") 
 
         # builds a dictionary for the first 10 reviews
@@ -84,7 +83,9 @@ def get_reviews(hyphen_restaurants, city):
         # Note there are some descrepancies between the number of reviews scraped from the Yelp page header and the number 
         # returned from the scraping. I.e. sweet potato sensations has 135 reviews according to yelp, but the code returns 138 reviews.
         # This is due to the double counting of multiple reviews - let's say a user reviews a restaurant in 2014, then they review 
-        # again in 2021, these reviews would be linked and the original review would be double counted 
+        # again in 2021, these reviews would be linked and the original review would be double counted.
+        # Tried a couple of different duplicate remvoal strategies, but all of them resulted in lost data
+        # If we do analysis with the resulatant csv files, prbably easiest to manually remove duplicates in excel
         counter = 10
         while num_reviews > counter:
             new_url = url + f"?start={str(counter)}"
@@ -103,11 +104,10 @@ def get_reviews(hyphen_restaurants, city):
                 reviews_list.append(review_d)
 
             counter += 10
-
         review_information['Yelp user reviews'] = reviews_list
-
         reviews[restaurant] = review_information
 
+        # writes csv files for each restaurant
         filename = f"review_csvs/{restaurant}.csv"
         with open(filename, "w") as csvfile:
             csvwriter = csv.writer(csvfile)
@@ -116,7 +116,6 @@ def get_reviews(hyphen_restaurants, city):
                 rat = subd['rating']
                 rev = subd['review']
                 csvwriter.writerow([rat, rev])
-
         print(f'done with {restaurant}')
 
     # prints number of reviews and the number of reviews scraped to see how many duplicates we have
@@ -126,11 +125,14 @@ def get_reviews(hyphen_restaurants, city):
         print(reviews[key]['number of reviews'])
         print(len(reviews[key]['Yelp user reviews']))
 
+    return non_valid
+
 def main():
     print('starting')
     restaurants = get_black_owned()
     hyphenated_restaurants = hyphenate(restaurants)
-    get_reviews(hyphenated_restaurants, 'detroit')
+    non_valid = get_reviews(hyphenated_restaurants, 'detroit')
+    print(f"not valid restaurants: {non_valid}")
     print('done')
 
 if __name__ == '__main__':
